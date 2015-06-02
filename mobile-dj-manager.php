9c72f58715f6320fb8880b850c739eca2b8916e5
mikeyhoward1977 <mikeyhoward1977@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 18:38:54 +0000

    versioning
    git-svn-id: http://plugins.svn.wordpress.org/mobile-dj-manager/trunk@1173007 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mobile-dj-manager.php
+++ mobile-dj-manager.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Mobile DJ Manager
 Description: Mobile DJ Manager is an interface allowing mobile DJ's and businesses to manage their events and employees as well as interact with their clients easily. Automating many of your day to day tasks, Mobile DJ Manager for WordPress is the ultimate tool for any Mobile DJ Business.
-Version: 1.1.5
+Version: 1.2
 Date: 17 March 2015
 Author: My DJ Planner <contact@mydjplanner.co.uk>
 Author URI: http://www.mydjplanner.co.uk
