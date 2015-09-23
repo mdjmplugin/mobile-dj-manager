d3a5de4c403f791f8e4dcad22b22835bd3f3f1b3
mikeyhoward1977 <mikeyhoward1977@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-23 09:03:38 +0000

    1.2.4.1 Upgrade script
    git-svn-id: http://plugins.svn.wordpress.org/mobile-dj-manager/trunk@1251864 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/includes/procedures/update_to_1.2.4.1.php
+++ admin/includes/procedures/update_to_1.2.4.1.php
@@ -5,4 +5,30 @@
  *
  *
  */
-	$GLOBALS['mdjm_debug']->log_it( 'No update procedures required for version ' . MDJM_VERSION_NUM );
+	class MDJM_Upgrade_to_1_2_4_1	{
+		function __construct()	{
+			$this->update_settings();
+		}
+		
+		/*
+		 * Update MDJM settings
+		 *
+		 *
+		 *
+		 */
+		function update_settings()	{
+			
+			$GLOBALS['mdjm_debug']->log_it( 'Updating Settings for version 1.2.4.1' );
+						
+			$status = get_option( '__mydj_validation' );
+			
+			if( $status['key'] == 'XXXX' || $status['type'] = 'trial' || time() >= strtotime( $status['expire'] ) )
+				add_option( 'mdjm_price_warn', 1, '', 'yes' );
+			
+			$GLOBALS['mdjm_debug']->log_it( 'Settings Updated' );
+						
+		} // update_settings
+				
+	} // class MDJM_Upgrade_to_1_2_4_1
+	
+	new MDJM_Upgrade_to_1_2_4_1();
