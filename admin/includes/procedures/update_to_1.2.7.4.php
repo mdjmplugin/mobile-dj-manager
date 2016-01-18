5e31e372f4238ef50c128a8702505ac19eb3de33
mikeyhoward1977 <mikeyhoward1977@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-01-18 12:11:28 +0000

    Removed duplicate client fields on profile screen
    git-svn-id: http://plugins.svn.wordpress.org/mobile-dj-manager/trunk@1330447 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/includes/procedures/update_to_1.2.7.4.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Update procedures for version 1.2.7.4
+ *
+ *
+ *
+ */
+	class MDJM_Upgrade_to_1_2_7_4	{
+		function __construct()	{
+			$GLOBALS['mdjm_debug']->log_it( 'No updates required for version 1.2.7.4' );
+		}
+	}
+	
+	new MDJM_Upgrade_to_1_2_7_4();
