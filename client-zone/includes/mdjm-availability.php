be75ef7a200182f2db84ff17bda5b150d362f034
mikeyhoward1977 <mikeyhoward1977@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-01-18 12:09:09 +0000

    Issue #1. Date now carries across to contact form if available
    git-svn-id: http://plugins.svn.wordpress.org/mobile-dj-manager/trunk@1330440 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- client-zone/includes/mdjm-availability.php
+++ client-zone/includes/mdjm-availability.php
@@ -57,6 +57,9 @@ if( !class_exists( 'MDJM_Availability_Checker' ) ) :
 		 *
 		 */
 		public static function ajax_in_head()	{
+			if( empty( self::$ajax ) )
+				return;
+			
 			global $mdjm, $mdjm_settings;
 				
 			if( $mdjm_settings['availability']['availability_check_pass_page'] != 'text' )
@@ -91,7 +94,7 @@ if( !class_exists( 'MDJM_Availability_Checker' ) ) :
 								<?php
 								if( !empty( $pass_redirect ) )	{
 									?>
-									window.location.href = '<?php echo $mdjm->get_link( $mdjm_settings['availability']['availability_check_pass_page'], true ); ?>';
+									window.location.href = '<?php echo $mdjm->get_link( $mdjm_settings['availability']['availability_check_pass_page'], true ); ?>mdjm_avail_date=' + check_date;
 									<?php
 								}
 								else	{
