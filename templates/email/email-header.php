94c349249c3bdf57382863c84531be38c812ec6d
Mike Howard <mike@mikeandniki.co.uk>
2016-04-27 14:31:28 +0100

    Added email templates

---
+++ templates/email/email-header.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This template is used to display the header content of an email.
+ *
+ * @version			1.0
+ * @author			Mike Howard
+ * @since			1.3
+ * @content_tag		client
+ * @content_tag		event
+ * @shortcodes		Not Supported
+ *
+ * Do not customise this file!
+ * If you wish to make changes, copy this file to your theme directory /theme/mdjm-templates/email/email-header.php
+ */
+if ( ! defined( 'ABSPATH' ) )
+	exit; 
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>{company_name}</title>
+	</head>
+	<body>
