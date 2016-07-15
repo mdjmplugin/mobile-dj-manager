258b7ac4ba4f23e1ef309255bb69d42584e13c01
Mike Howard <mike@mikeandniki.co.uk>
2016-07-15 21:27:36 +0100

    User validation

--- includes/api/class-mdjm-api.php
+++ includes/api/class-mdjm-api.php
@@ -2,7 +2,7 @@
 /**
  * MDJM Rest API
  *
- * Extends the WordPress REST API to provide an API interface.
+ * Provides an API REST interface.
  *
  * The primary purpose of this class is for availability checking and
  * event queries.
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  *
  * @since	1.4
  */
-class MDJM_API extends WP_REST_Controller 	{
+class MDJM_API 	{
 
 	/**
 	 * Latest API Version
@@ -32,7 +32,7 @@ class MDJM_API extends WP_REST_Controller 	{
 	const VERSION = 1;
 
 	/**
-	 * Namespace
+	 * Namespace.
 	 */
 	public $namespace;
 
@@ -40,10 +40,19 @@ class MDJM_API extends WP_REST_Controller 	{
 	 * Log API requests?
 	 *
 	 * @var		bool
-	 * @access	private
+	 * @access	public
 	 * @since	1.4
 	 */
 	public $log_requests = true;
+
+	/**
+	 * Request data.
+	 *
+	 * @var		arr
+	 * @access	public
+	 * @since	1.4
+	 */
+	private $request = array();
 
 	/**
 	 * Is this a valid request?
@@ -55,7 +64,7 @@ class MDJM_API extends WP_REST_Controller 	{
 	private $is_valid_request = false;
 
 	/**
-	 * User ID Performing the API Request
+	 * User ID Performing the API Request.
 	 *
 	 * @var		int
 	 * @access	private
@@ -64,7 +73,16 @@ class MDJM_API extends WP_REST_Controller 	{
 	public $user_id = 0;
 
 	/**
-	 * Setup the MDJM API
+	 * Response data to return
+	 *
+	 * @var		array
+	 * @access	private
+	 * @since	1.4
+	 */
+	private $data = array();
+
+	/**
+	 * Setup the MDJM API.
 	 *
 	 * @since	1.4
 	 */
@@ -86,13 +104,7 @@ class MDJM_API extends WP_REST_Controller 	{
 		if ( $endpoints )	{
 
 			foreach( $endpoints as $base => $args )	{
-
-				$register_endpoint = register_rest_route(
-					$namespace,
-					$base,
-					$args
-				);
-
+				register_rest_route( $this->namespace, $base, $args, false );
 			}
 
 		}
@@ -108,42 +120,123 @@ class MDJM_API extends WP_REST_Controller 	{
 	public function define_endpoints()	{
 
 		$endpoints = array(
-			'/affiliate/(?P<api_key>[\w-]+)' => array(
-				'methods'  => WP_REST_Server::READABLE,
-				'callback' => 'mdjm_aff_authenticate_api',
-				'args'     => array(
-					'api_key' => array(
-						'required'          => true
-					)
-				)
+			// For checking agency availability
+			'/availability/' => array(
+				'methods'  => array( WP_REST_Server::READABLE ),
+				'callback' => array( $this, 'process_request' ),
 			),
-			'/availability/(?P<date>[\w-]+)' => array(
-				'methods'  => array( WP_REST_Server::READABLE, WP_REST_Server::CREATABLE ),
-				'callback' => array( $this, 'availability_check' ),
-				'args'     => array(
-					'date' => array(
-						'required'          => true
-					)
-				)
-			),
+			// For retrieving and managing affiliated events
 			'/events/' => array(
-				'methods'  => array( WP_REST_Server::READABLE, WP_REST_Server::CREATABLE ),
-				'callback' => array( $this, '' ),
-				'args'     => array(
-					'date' => array(
-						'required'          => true
-					)
-				)
-			),
-			'/events/create' => array(
-				'methods'  => array( WP_REST_Server::WRITEABLE, WP_REST_Server::CREATABLE ),
-				'callback' => array( $this, '' )
+				'methods'  => array( WP_REST_Server::READABLE ),
+				'callback' => array( $this, 'process_request' ),
 			)
 		);
 
 		return apply_filters( 'mdjm_api_endpoints', $endpoints );
 
 	} // define_endpoints
+
+	/**
+	 * Process API requests.
+	 *
+	 * @since	1.4
+	 * @param	arr		$request	API Request data.
+	 * @return	void
+	 */
+	public function process_request( WP_REST_Request $request )	{
+
+		$start = microtime( true ); // Start time for logging.
+
+		$this->request = $request;
+
+		$this->validate_request();
+
+	} // process_request
+
+	/**
+	 * Validate the current request.
+	 *
+	 * @access	private
+	 * @since	1.4
+	 * @return	void
+	 */
+	private function validate_request()	{
+
+		if ( empty( $this->request['api_key'] ) || empty( $this->request['token'] ) )	{
+
+			$this->missing_auth();
+
+		} elseif ( ! ( $user = $this->get_user() ) )	{
+
+			$this->invalid_key();
+
+		} else	{
+
+			$public = $this->request>get_param( 'api_key' );
+			$token  = $this->request->get_param( 'token' );
+			$secret = $this->get_user_secret_key( $user );
+
+			if ( hash_equals( md5( $secret . $public ), $token ) ) {
+				$this->is_valid_request = true;
+			} else {
+				$this->invalid_auth();
+			}
+
+		}
+
+	} // validate_request
+
+	/**
+	 * Sends a response to the API request.
+	 *
+	 * @since	1.4
+	 * @param	int		$status_code	Status code.
+	 * @return	void
+	 */
+	public function output( $status_code = 200 )	{
+
+		$response = new WP_REST_Response( $this->data );
+		$response->set_status( $status_code );
+		$response->header( 'Content-type', 'application/json' );
+		
+		return $response;
+	} // output
+
+	/**
+	 * Retrieve a user ID from the API key provided.
+	 *
+	 * @since	1.4
+	 * @global	obj		$wpdb
+	 * @param	str		$api_key	The API from which to retrieve the user.
+	 * @return	void
+	 */
+	public function get_user()	{
+		global $wpdb;
+
+		if ( empty( $key ) ) {
+			$key = $this->request->get_param( 'api_key' );
+		}
+
+		if ( empty( $key ) ) {
+			return false;
+		}
+
+		$user = get_transient( md5( 'mdjm_api_user_' . $key ) );
+
+		if ( false === $user ) {
+			$user = $wpdb->get_var( $wpdb->prepare( "SELECT user_id FROM $wpdb->usermeta WHERE meta_key = 'mdjm_user_public_key' AND meta_value = %s LIMIT 1", $key ) );
+
+			set_transient( md5( 'mdjm_api_user_' . $key ) , $user, DAY_IN_SECONDS );
+		}
+
+		if ( $user != NULL ) {
+			$this->user_id = $user;
+			return $user;
+		}
+
+		return false;
+
+	} // get_user
 
 	/**
 	 * Prepare and execute an availability check.
@@ -182,5 +275,53 @@ class MDJM_API extends WP_REST_Controller 	{
 		return $response;
 	
 	} // availability_check
+
+	/**
+	 * Displays an authentication error if api key is invalid.
+	 *
+	 * @since	1.4
+	 * @access	private
+	 * @uses	MDJM_API::output()
+	 * @return	void
+	 */
+	private function invalid_key() {
+		$error = array();
+		$error['error'] = __( 'Invalid API key.', 'mobile-dj-manager' );
+
+		$this->data = $error;
+		$this->output( 403 );
+	} // invalid_key
+
+	/**
+	 * Displays a missing authentication error if required paramaters are not provided.
+	 *
+	 * @since	1.4
+	 * @access	private
+	 * @uses	MDJM_API::output()
+	 * @return	void
+	 */
+	private function missing_auth() {
+		$error = array();
+		$error['error'] = __( 'No API key provided.', 'mobile-dj-manager' );
+
+		$this->data = $error;
+		$this->output( 401 );
+	} // missing_auth
+
+	/**
+	 * Displays an authentication error if credentials are invalid.
+	 *
+	 * @since	1.4
+	 * @access	private
+	 * @uses	MDJM_API::output()
+	 * @return	void
+	 */
+	private function invalid_auth() {
+		$error = array();
+		$error['error'] = __( 'Authentication failed.', 'mobile-dj-manager' );
+
+		$this->data = $error;
+		$this->output( 403 );
+	} // invalid_auth
 
 } // MDJM_API
