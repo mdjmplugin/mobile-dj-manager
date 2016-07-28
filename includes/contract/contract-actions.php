65598187da00745103099b52b70d1e9833a8d43e
Mike Howard <mike@mikeandniki.co.uk>
2016-07-28 11:29:24 +0100

    Force validation of the checkboxes on contract signing. Fixes #126

--- includes/contract/contract-actions.php
+++ includes/contract/contract-actions.php
@@ -48,14 +48,15 @@ function mdjm_sign_event_contract_action( $data )	{
 	$user = wp_get_current_user();
 	
 	$password_confirmation = wp_authenticate( $user->user_login, $data['mdjm_verify_password'] );
-	
+
+	$data['mdjm_accept_terms']   = ! empty( $data['mdjm_accept_terms'] )   ? $data['mdjm_accept_terms']   : false;
+	$data['mdjm_confirm_client'] = ! empty( $data['mdjm_confirm_client'] ) ? $data['mdjm_confirm_client'] : false;
+
 	if( is_wp_error( $password_confirmation ) )	{
 		$message = 'password_error';
-	}
-	elseif( ! wp_verify_nonce( $data[ 'mdjm_nonce' ], 'sign_contract' ) )	{
+	} elseif( ! wp_verify_nonce( $data[ 'mdjm_nonce' ], 'sign_contract' ) )	{
 		$message = 'nonce_fail';
-	}
-	else	{
+	} else	{
 		// Setup the signed contract details
 		$posted = array();
 	
