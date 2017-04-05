9971f88c18116356e9f062eef900d6407d9afd24
Mike Howard <mike@mikeandniki.co.uk>
2017-04-05 12:18:01 +0100

    Updated settings page display for licenses

--- includes/admin/settings/display-settings.php
+++ includes/admin/settings/display-settings.php
@@ -35,7 +35,7 @@ function mdjm_options_page() {
 	$section             = isset( $_GET['section'] ) && ! empty( $registered_sections ) && array_key_exists( $_GET['section'], $registered_sections ) ? $_GET['section'] : $key;
 	ob_start();
 	?>
-	<div class="wrap">
+	<div class="wrap <?php echo 'wrap-' . $active_tab; ?>">
 		<h1 class="nav-tab-wrapper">
 			<?php
 			foreach( mdjm_get_settings_tabs() as $tab_id => $tab_name ) {
