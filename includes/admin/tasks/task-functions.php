f5254880e41ffbfb1268a5d60fae96ca1541363c
Mike Howard <mike@mikeandniki.co.uk>
2017-04-05 18:48:54 +0100

    Added task detail page

--- includes/admin/tasks/task-functions.php
+++ includes/admin/tasks/task-functions.php
@@ -27,6 +27,63 @@ function mdjm_get_tasks()	{
 } // mdjm_get_tasks
 
 /**
+ * Retrieve a single tasks
+ *
+ * @since	1.4.7
+ * @param	str		$id		The ID of the task
+ * @return	arr		Array of tasks
+ */
+function mdjm_get_task( $id )	{
+	$tasks = mdjm_get_tasks();
+
+	if ( array_key_exists( $id, $tasks ) )	{
+		return $tasks[ $id ];
+	}
+
+	return false;
+} // mdjm_get_task
+
+/**
+ * Whether or not a task can be deleted
+ *
+ * @since	1.4.7
+ * @param	str|arr		$task		A task ID, or array
+ * @return	arr		Array of tasks
+ */
+function mdjm_can_delete_task( $task )	{
+	if ( ! is_array( $task ) )	{
+		$task = mdjm_get_task( $task );
+	}
+
+	if ( empty( $task['default'] ) )	{
+		return true;
+	}
+
+	return false;
+} // mdjm_get_task
+
+/**
+ * Retrieve schedule options for tasks
+ *
+ * @since	1.4.7
+ * @return	arr		Array of options
+ */
+function mdjm_get_task_schedule_options()	{
+	$schedules = array(
+		'Hourly'      => 'Hourly',
+		'Daily'       => 'Daily',
+        'Twice Daily' => 'Twice Daily',
+        'Weekly'      => 'Weekly',
+        'Monthly'     => 'Monthly',
+        'Yearly'      => 'Yearly',
+	);
+
+	$schedules = apply_filters( 'mdjm_task_schedule_options', $schedules );
+
+	return $schedules;
+} // mdjm_get_task
+
+/**
  * Set the status for a given task
  *
  * @since	1.4.7
--- includes/admin/tasks/tasks-page.php
+++ includes/admin/tasks/tasks-page.php
@@ -22,14 +22,8 @@ if ( ! defined( 'ABSPATH' ) )
  * @return	void
 */
 function mdjm_tasks_page() {
-	$requested_view = isset( $_GET['view'] ) ? sanitize_text_field( $_GET['view'] ) : 'tasks';
-
-	if ( array_key_exists( $requested_view, $default_views ) && function_exists( $default_views[ $requested_view ] ) ) {
-		if ( 'view' == $requested_view )	{
-			mdjm_render_single_task_view();
-		} else	{
-			wp_die( __( 'Page not found', 'mobile-dj-manager' ) );
-		}
+	if ( isset( $_GET['view'], $_GET['id'] ) && 'task' == sanitize_text_field( $_GET['view'] ) ) {
+			mdjm_render_single_task_view( $_GET['id'] );
 	} else {
 		mdjm_tasks_list();
 	}
@@ -73,23 +67,136 @@ function mdjm_tasks_list() {
  * @param	arr		$callbacks	The Registered views and their callback functions
  * @return	void
  */
-function mdjm_render_single_task_view( $view, $callbacks ) {
+function mdjm_render_single_task_view( $id ) {
 
-	$id             = isset( $_GET['id'] ) ? $_GET['id'] : 0;
 	$task_view_role = apply_filters( 'mdjm_view_tasks_role', 'manage_mdjm' );
 	$url            = remove_query_arg( array( 'mdjm-message', 'render' ) );
+	$task           = mdjm_get_task( $id );
+	$return_url     = add_query_arg( array(
+		'post_type' => 'mdjm-event',
+		'page'      => 'mdjm-tasks'
+	), admin_url( 'edit.php' ) );
+
+	if ( empty( $task ) )	{
+		wp_die( __( 'Invalid task', 'mobile-dj-manager' ) );
+	}
+
+	$delete_url = add_query_arg( array(
+		'post_type'   => 'mdjm-event',
+		'page'        => 'mdjm-tasks',
+		'mdjm-action' => 'delete_task',
+		'task_id' => $id
+		), admin_url( 'edit.php?' )
+	);
 
 	?>
 
-	<div class='wrap'>
+	<div class="wrap mdjm-wrap">
+        <h2><?php printf( __( 'Task: %s', 'mobile-dj-manager' ), esc_html( $task['name'] ) ); ?></h2>
+        <?php do_action( 'mdjm_view_task_details_before', $id ); ?>
+        <form id="mdjm-edit-task-form" method="post">
+		<?php do_action( 'mdjm_view_task_details_form_top', $id ); ?>
+        <div id="poststuff">
+			<div id="mdjm-dashboard-widgets-wrap">
+				<div id="post-body" class="metabox-holder columns-2">
+					<div id="postbox-container-1" class="postbox-container">
+						<div id="side-sortables" class="meta-box-sortables ui-sortable">
 
-		<?php if ( $customer && $render ) : ?>
-            <div id="kbs-item-wrapper" class="kbs-customer-wrapper" style="float: left">
-                <?php $callbacks[ $view ]( $customer ) ?>
-            </div>
-        <?php endif; ?>
+							<?php do_action( 'mdjm_view_task_details_sidebar_before', $id ); ?>
 
-	</div><!-- .wrap -->
+							<div id="mdjm-task-update" class="postbox mdjm-task-data">
+
+								<h3 class="hndle">
+									<span><?php _e( 'Update Task', 'mobile-dj-manager' ); ?></span>
+								</h3>
+
+								<div class="mdjm-task-update-box mdjm-admin-box">
+									<?php do_action( 'mdjm_view_task_details_update_before', $id ); ?>
+									<div id="major-publishing-actions">
+                                    	<?php if ( mdjm_can_delete_task( $task ) ) : ?>
+                                            <div id="delete-action">
+                                                <a href="<?php echo wp_nonce_url( $delete_url, 'mdjm_task_nonce' ) ?>" class="mdjm-delete-task mdjm-delete">
+                                                    <?php _e( 'Delete Task', 'mobile-dj-manager' ); ?>
+                                                </a>
+                                            </div>
+                                        <?php endif; ?>
+										<input type="submit" class="button button-primary right" value="<?php esc_attr_e( 'Save Task', 'mobile-dj-manager' ); ?>"/>
+										<div class="clear"></div>
+									</div>
+									<?php do_action( 'mdjm_view_task_details_update_after', $id ); ?>
+								</div><!-- /.mdjm-order-update-box -->
+
+							</div><!-- /#mdjm-task-data -->
+
+							<?php do_action( 'mdjm_view_task_details_sidebar_after', $id ); ?>
+
+						</div><!-- /#side-sortables -->
+					</div><!-- /#postbox-container-1 -->
+
+					<div id="postbox-container-2" class="postbox-container">
+						<div id="normal-sortables" class="meta-box-sortables ui-sortable">
+
+							<?php do_action( 'mdjm_view_task_details_main_before', $id ); ?>
+
+							<div id="mdjm-task-details" class="postbox">
+								<h3 class="hndle">
+									<span><?php _e( 'Task Details', 'mobile-dj-manager' ); ?></span>
+								</h3>
+								<div class="inside mdjm-clearfix">
+
+									<div class="column-container task-info">
+										<div class="column">
+                                        	<strong><?php _e( 'Name:', 'mobile-dj-manager' ); ?></strong>
+                                            <br />
+											<?php echo MDJM()->html->text( array(
+												'id'    => 'mdjm-task-name',
+												'name'  => 'task_name',
+												'value' => esc_html( $task['name'] ),
+												'class' => 'regular-text',
+											) ); ?>
+										</div>
+                                        <div class="column column-2">
+                                        	<strong><?php _e( 'Frequency:', 'mobile-dj-manager' ); ?></strong>
+                                            <br />
+											<?php echo MDJM()->html->select( array(
+												'options'          => mdjm_get_task_schedule_options(),
+												'name'             => 'task_frequency',
+												'id'               => 'mdjm-task-frequency',
+												'selected'         => $task['frequency']
+											) ); ?>
+										</div>
+									</div>
+
+									<div class="column-container task-info">
+                                        <p><strong><?php _e( 'Description:', 'mobile-dj-manager' ); ?></strong>
+                                        <br />
+                                        <?php echo MDJM()->html->textarea( array(
+                                            'name'        => 'task_description',
+											'value'       => esc_html( $task['desc'] ),
+											'class'       => 'large-text description'
+                                        ) ); ?></p>
+									</div>
+
+									<?php
+									do_action( 'mdjm_task_view_details', $id );
+									?>
+
+								</div><!-- /.inside -->
+							</div><!-- /#mdjm-task-details -->
+
+							<?php do_action( 'mdjm_view_task_details_main_after', $id ); ?>
+						</div><!-- /#normal-sortables -->
+					</div><!-- #postbox-container-2 -->
+				</div><!-- /#post-body -->
+			</div><!-- #mdjm-dashboard-widgets-wrap -->
+		</div><!-- /#post-stuff -->
+		<?php do_action( 'mdjm_view_task_details_form_bottom', $id ); ?>
+		<?php wp_nonce_field( 'mdjm_update_task_details_nonce' ); ?>
+		<input type="hidden" name="mdjm_task_id" value="<?php echo esc_attr( $id ); ?>"/>
+        <input type="hidden" name="mdjm-action" value="update_task_details"/>
+	</form>
+	<?php do_action( 'mdjm_view_task_details_after', $id ); ?>
+</div><!-- /.wrap -->
 	
 	<?php
 
