48de7f2b2f5862e75a6bfb17b6c6ad9db53f57c3
mikeyhoward1977 <mike@mikeandniki.co.uk>
2018-05-30 18:02:40 +0100

    Start of Privacy functions

--- mobile-dj-manager.php
+++ mobile-dj-manager.php
@@ -196,6 +196,7 @@ if( ! class_exists( 'Mobile_DJ_Manager' ) ) :
 			require_once( MDJM_PLUGIN_DIR . '/includes/txns/txn-actions.php' );
 			require_once( MDJM_PLUGIN_DIR . '/includes/equipment/equipment-functions.php' );
 			require_once( MDJM_PLUGIN_DIR . '/includes/misc-functions.php' );
+            require_once( MDJM_PLUGIN_DIR . '/includes/privacy-functions.php' );
 			require_once( MDJM_PLUGIN_DIR . '/includes/admin/pages/event-fields.php' );
 			require_once( MDJM_PLUGIN_DIR . '/includes/admin/pages/client-fields.php' );
 			require_once( MDJM_PLUGIN_DIR . '/includes/admin/users/class-mdjm-users.php' );
