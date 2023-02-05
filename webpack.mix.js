8f9a36495eae03861c3864ebaadcc6552c6ff6bb
Dan Porter <porterdan91@gmail.com>
2023-02-05 15:14:23 +0000

    Added admin mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,11 @@
 let mix = require('laravel-mix');
+
+// Frontend.
 mix.js(['src/js/main.js'], 'assets/js/mdjm-plugin.js');
 mix.sass('src/sass/main.scss', 'assets/css/mdjm-plugin.css');
 mix.minify(['assets/css/mdjm-plugin.css', 'assets/js/mdjm-plugin.js']);
+
+// Admin.
+mix.js(['src/admin/js/admin.js'], 'assets/js/mdjm-admin.js');
+mix.sass('src/admin/sass/admin.scss', 'assets/css/mdjm-admin.css');
+mix.minify(['assets/css/mdjm-admin.css', 'assets/js/mdjm-admin.js']);
